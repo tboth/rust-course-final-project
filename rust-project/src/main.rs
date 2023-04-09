8eac0ecabbea98f2c2fdb1e515d4a9694345a131
Pavol Hradský <71588191+PavolHradsky@users.noreply.github.com>
2023-04-09 15:30:47 +0200

    rozrobene templaty

--- rust-project/src/main.rs
+++ rust-project/src/main.rs
@@ -1,8 +1,15 @@
+use std::fs::File;
+
+use rocket_dyn_templates::{Template, context};
+// use rocket_static_files::StaticFiles;
+// use rocket_contrib::serve::StaticFiles;
+use rocket::fs::FileServer;
+
 #[macro_use] extern crate rocket;
 
 #[get("/")]
-fn index() -> &'static str {
-    "Hello, world!"
+fn index() -> Template {
+    Template::render("index", context! {field: "value"})
 }
 
 #[get("/test")]
@@ -13,5 +20,7 @@ fn test_page() -> &'static str {
 #[launch]
 fn rocket() -> _ {
     rocket::build()
+        .attach(Template::fairing())
+        .mount("/static", FileServer::from("static"))
         .mount("/", routes![index, test_page])
 }
