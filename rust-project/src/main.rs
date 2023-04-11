6a2025d73865ff27b48ede3948d977afd0a43f91
Pavol Hradský <71588191+PavolHradsky@users.noreply.github.com>
2023-04-11 14:42:20 +0200

    added templates and styles

--- rust-project/src/main.rs
+++ rust-project/src/main.rs
@@ -1,8 +1,4 @@
-use std::fs::File;
-
 use rocket_dyn_templates::{Template, context};
-// use rocket_static_files::StaticFiles;
-// use rocket_contrib::serve::StaticFiles;
 use rocket::fs::FileServer;
 
 #[macro_use] extern crate rocket;
@@ -10,6 +6,26 @@ use rocket::fs::FileServer;
 #[get("/")]
 fn index() -> Template {
     Template::render("index", context! {field: "value"})
+}
+
+#[get("/login")]
+fn login() -> Template {
+    Template::render("login", context! {field: "value"})
+}
+
+#[get("/register")]
+fn register() -> Template {
+    Template::render("register", context! {field: "value"})
+}
+
+#[get("/post")]
+fn post() -> Template {
+    Template::render("post", context! {field: "value"})
+}
+
+#[get("/addpost")]
+fn addpost() -> Template {
+    Template::render("addpost", context! {field: "value"})
 }
 
 #[get("/test")]
@@ -22,5 +38,5 @@ fn rocket() -> _ {
     rocket::build()
         .attach(Template::fairing())
         .mount("/static", FileServer::from("static"))
-        .mount("/", routes![index, test_page])
+        .mount("/", routes![index, test_page, login, register, post, addpost])
 }
