d16b7d5a71166f3a99636ee20783e6d51dfb1676
Tomas Both <bothtipp@gmail.com>
2023-04-13 11:16:43 +0200

    Connect seaorm db to rocket

--- rust-project/src/main.rs
+++ rust-project/src/main.rs
@@ -1,41 +1,56 @@
 use rocket_dyn_templates::{Template, context};
 use rocket::fs::FileServer;
+use rocket::State;
+use sea_orm::*;
+
+const DATABASE_URL: &str = "sqlite:./sqlite.db?mode=rwc";
+
+pub async fn set_up_db() -> Result<DatabaseConnection, DbErr> {
+    let db = Database::connect(DATABASE_URL).await?;
+    Ok(db)
+}
 
 #[macro_use] extern crate rocket;
 
 #[get("/")]
-fn index() -> Template {
+fn index(db: &State<DatabaseConnection>) -> Template {
     Template::render("index", context! {field: "value"})
 }
 
 #[get("/login")]
-fn login() -> Template {
+fn login(db: &State<DatabaseConnection>) -> Template {
     Template::render("login", context! {field: "value"})
 }
 
 #[get("/register")]
-fn register() -> Template {
+fn register(db: &State<DatabaseConnection>) -> Template {
     Template::render("register", context! {field: "value"})
 }
 
 #[get("/post")]
-fn post() -> Template {
+fn post(db: &State<DatabaseConnection>) -> Template {
     Template::render("post", context! {field: "value"})
 }
 
 #[get("/addpost")]
-fn addpost() -> Template {
+fn addpost(db: &State<DatabaseConnection>) -> Template {
     Template::render("addpost", context! {field: "value"})
 }
 
 #[get("/test")]
-fn test_page() -> &'static str {
+fn test_page(db: &State<DatabaseConnection>) -> &'static str {
     "This is test page"
 }
 
 #[launch]
-fn rocket() -> _ {
+async fn rocket() -> _ {
+    let db = match set_up_db().await {
+        Ok(db) => db,
+        Err(err) => panic!("{}", err),
+    };
+
     rocket::build()
+        .manage(db)
         .attach(Template::fairing())
         .mount("/static", FileServer::from("static"))
         .mount("/", routes![index, test_page, login, register, post, addpost])
